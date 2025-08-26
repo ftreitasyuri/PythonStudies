6e148568ffd54b5a6149897910fb8dc07811536a
ftreitasyuri <yuriftq@gmail.com>
2025-08-26 14:53:48 -0300

    Upload: Parte 1/3 finalizada: Agora já conseguimos percorrer os pfds, resgatar as infos e savlar o excel

--- AutoLeituraPDFs/auto_invoices.py
+++ AutoLeituraPDFs/auto_invoices.py
@@ -1,6 +1,8 @@
 import os
 from openpyxl import Workbook
 import pdfplumber
+import re
+from datetime import datetime
 # Verificar quantos arquivos/pdf existem
 
 diretorio = 'pdf_invoices'
@@ -27,16 +29,54 @@ ws['C1'] = "Nome do Arquivo"
 ws['D1'] = 'Status'
 
 ultima_linha_vazia = 1
+# Loop para descobrir a última linha vazia na planilha, vamos usar para preencher com os dados das nfs
 while ws['A' + str(ultima_linha_vazia)].value is not None:
     ultima_linha_vazia += 1
     
-
+# Loop para percorrer e resgatar o conteúdo de cada pdf
 for arquivo in arquivos:
     with pdfplumber.open(diretorio + "/" + arquivo) as pdf:
         primeira_pagina = pdf.pages[0]
         texto_pdf = primeira_pagina.extract_text()
-        print(texto_pdf)
+        # Debug
+        # print(texto_pdf)
+        
+        # Regex para pegar apenas o conteúdo desejado
+    numero_nota_padra = r'INVOICE #(\d+)'
+    numero_data_padra = r'DATE (\d{2}/\d{2/\d{4}})'
+    
+    match_number = re.search(numero_data_padra, texto_pdf)
+    match_date = re.search(numero_data_padra, texto_pdf)
+    
+    # Verificar se foi encontrado
+    if match_number:
+        numero_nota = match_number.group(1)
+        ws['A' + str(ultima_linha_vazia)] = numero_nota
+    else:
+        ws['A' + str(ultima_linha_vazia)] = 'Não foi encontrado o número da nota'
+        
+    if match_date:
+        numero_data = match_date.group(1)
+        ws['B' + str(ultima_linha_vazia)] = numero_data
+    else:
+        ws['B' + str(ultima_linha_vazia)] = 'Não foi encontrada a data'
+        
+    ws['C' + str(ultima_linha_vazia)] = arquivo
+    ws['D' + str(ultima_linha_vazia)] = "Arquivo lido com sucesso!"
+    
+    ultima_linha_vazia += 1
+    
+# Pegando a data e hora para salvar o excel
 
+data_atual = str(datetime.now()).replace(":", "-")
+indice_ponto = data_atual.index(".")
+data_real = data_atual[:indice_ponto]
+    
+    
+    
+        
+        
+wb.save(f"Notas - {data_real}.xlsx")
 
 
 
