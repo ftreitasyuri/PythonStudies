b55b1e5d397e14f31ab9c045396354330cb6f99a
ftreitasyuri <yuriftq@gmail.com>
2025-08-30 14:02:52 -0300

    Upload: Subindo configuração que envia um arquivo tipo csv para o snowflake

--- SnowFlakeIntegration/main.py
+++ SnowFlakeIntegration/main.py
@@ -1,20 +1,52 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import pandas as pd
+import os
+from dotenv import load_dotenv
+import snowflake.connector
+from snowflake.connector.pandas_tools import write_pandas
 
-# 1. Ler planilha
-df = pd.read_excel("resultados.xlsx")
+# Carregando env
+load_dotenv()
 
-# 2. Conectar com SQLAlchemy
-engine = create_engine(
-    'snowflake://{user}:{password}@{account}/{database}/{schema}?warehouse={warehouse}'.format(
-        user="SEU_USUARIO",
-        password="SUA_SENHA",
-        account="SEU_ACCOUNT_ID",
-        database="SEU_DATABASE",
-        schema="SEU_SCHEMA",
-        warehouse="SEU_WAREHOUSE"
-    )
+
+# Recupenrando os valores do .env
+ctx = snowflake.connector.connect(
+    user=os.getenv("SNOWFLAKE_USER"),
+    password=os.getenv("SNOWFLAKE_PASSWORD"),
+    account=os.getenv("SNOWFLAKE_ACCOUNT"),
+    warehouse=os.getenv("SNOWFLAKE_WAREHOUSE"),
+    database=os.getenv("SNOWFLAKE_DATABASE"),
+    schema=os.getenv("SNOWFLAKE_SCHEMA"),
+    role=os.getenv("SNOWFLAKE_ROLE")
 )
 
-# 3. Enviar dataframe
-df.to_sql('checklist', engine, if_exists='append', index=False)
+
+# 2. Ler planilha
+df = pd.read_csv("checklist_sample.csv")
+df["impressao_gerada"] = pd.to_datetime(df["impressao_gerada"], format="%d/%m/%Y %H:%M", errors="coerce")
+
+
+# 3. Garantir tabela existente
+with ctx.cursor() as cur:
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS CHECKLIST (
+        ARQUIVO STRING,
+        IMPRESSAO_GERADA TIMESTAMP_NTZ,
+        TITULO STRING
+    )
+    """)
+
+# Definindo os nomes das colunas como upercase
+df.columns = [c.upper() for c in df.columns]
+
+
+# 4. Carga otimizada com write_pandas
+success, nchunks, nrows, _ = write_pandas(
+    ctx,
+    df,
+    table_name="CHECKLIST"
+)
+
+print({"success": success, "chunks": nchunks, "rows_loaded": nrows})
+
+ctx.close()
